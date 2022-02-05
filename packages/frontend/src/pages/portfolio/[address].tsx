40dc2575db97e8090f4251f2467eb0981c1d8afb
Taiju Sanagi <taijusanagi@gmail.com>
2022-02-05 22:19:26 +0900

    chore: implement opensea api close #6

--- packages/frontend/src/pages/portfolio/[address].tsx
+++ packages/frontend/src/pages/portfolio/[address].tsx
@@ -1,5 +1,6 @@
 import { ethers } from "ethers";
 import type { GetServerSideProps, NextPage } from "next";
+import { Network, OpenSeaPort } from "opensea-js";
 
 import { PortfolioTemplate, PortfolioTemplateProps } from "../../components/templates/Portfolio";
 import { mockAccount, mockAssets } from "../../data/mock";
@@ -12,9 +13,25 @@ export default PortfolioPage;
 
 export const getServerSideProps: GetServerSideProps<PortfolioTemplateProps> = async ({ params }) => {
   const { address } = params as { address: string };
+  let assets = mockAssets;
   if (ethers.utils.isAddress(address)) {
     mockAccount.address = address;
     const provider = new ethers.providers.JsonRpcProvider(process.env.BLOCKCHAIN_RPC_URI);
+    const seaport = new OpenSeaPort(provider, {
+      networkName: Network.Main,
+      apiKey: process.env.OPENSEA_API_KEY,
+    });
+    const response = await seaport.api.getAssets({
+      owner: address,
+    });
+    assets = response.assets.map((asset) => {
+      return {
+        tokenId: asset.tokenId as string,
+        name: asset.name,
+        description: asset.description,
+        image: asset.imageUrl,
+      };
+    });
     const ens = await provider.lookupAddress(address);
     if (ens) {
       mockAccount.ens = ens;
@@ -24,7 +41,7 @@ export const getServerSideProps: GetServerSideProps<PortfolioTemplateProps> = as
     props: {
       accountProps: mockAccount,
       collectionProps: {
-        assets: mockAssets,
+        assets,
       },
     },
   };

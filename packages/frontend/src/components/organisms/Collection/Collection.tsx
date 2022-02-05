be301ec8766b5f57c450a1827e357ff7ab50f782
Taiju Sanagi <taijusanagi@gmail.com>
2022-02-06 00:35:58 +0900

    chore: added firebase

--- packages/frontend/src/components/organisms/Collection/Collection.tsx
+++ packages/frontend/src/components/organisms/Collection/Collection.tsx
@@ -1,4 +1,22 @@
-import { AspectRatio, Box, BoxProps, Image, SimpleGrid, Skeleton, Stack } from "@chakra-ui/react";
+import {
+  AspectRatio,
+  Box,
+  BoxProps,
+  Button,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  SimpleGrid,
+  Skeleton,
+  Stack,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
 
 import { Asset } from "../../../types/asset";
 
@@ -7,6 +25,15 @@ export interface CollectionProps extends BoxProps {
 }
 
 export const Collection: React.VFC<CollectionProps> = ({ assets, ...props }) => {
+  const [selectedAssetIndex, setSelectedAssetIndex] = React.useState(0);
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const openModal = (selectedAssetIndex: number) => {
+    setSelectedAssetIndex(selectedAssetIndex);
+    onOpen();
+  };
+
   return (
     <Box mx="auto" {...props}>
       <SimpleGrid columns={{ base: 3, lg: 4 }} gap="4">
@@ -14,12 +41,41 @@ export const Collection: React.VFC<CollectionProps> = ({ assets, ...props }) => 
           <Stack key={i} spacing="4">
             <Box position="relative" className="group">
               <AspectRatio ratio={1}>
-                <Image src={asset.image} alt={asset.name} draggable="false" fallback={<Skeleton />} />
+                <Image
+                  src={asset.image}
+                  alt={asset.name}
+                  onClick={() => openModal(i)}
+                  draggable="false"
+                  fallback={<Skeleton />}
+                />
               </AspectRatio>
             </Box>
           </Stack>
         ))}
       </SimpleGrid>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        motionPreset="slideInBottom"
+        scrollBehavior="inside"
+        size="lg"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Image
+              src={assets[selectedAssetIndex].image}
+              alt={assets[selectedAssetIndex].name}
+              draggable="false"
+              fallback={<Skeleton />}
+            />
+          </ModalBody>
+          <ModalFooter></ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };

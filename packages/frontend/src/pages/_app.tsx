89bed104c60113d79c1d85e2e72887487cd278b5
Taiju Sanagi <taijusanagi@gmail.com>
2022-02-05 21:38:29 +0900

    chore: integrate ens

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import type { AppProps } from "next/app";
+import Error from "next/error";
 
 import { AppWrapper } from "../components/utils/AppWapper";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <AppWrapper>
-      <Component {...pageProps} />
-    </AppWrapper>
-  );
+  if (pageProps.error) {
+    return <Error statusCode={pageProps.error.statusCode} title={pageProps.error.message} />;
+  } else {
+    return (
+      <AppWrapper>
+        <Component {...pageProps} />
+      </AppWrapper>
+    );
+  }
 };
 export default MyApp;

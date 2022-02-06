c8a2fb11c04aad5013d319fdaf1e7404c25b61f4
Taiju Sanagi <taijusanagi@gmail.com>
2022-02-06 12:24:27 +0900

    chore: implemented twitter login close #5

--- packages/frontend/src/components/organisms/Header/Header.tsx
+++ packages/frontend/src/components/organisms/Header/Header.tsx
@@ -1,7 +1,9 @@
-import { Box, BoxProps, Flex, Text } from "@chakra-ui/react";
+import { Box, BoxProps, Button, Flex, Text } from "@chakra-ui/react";
+import { getAuth, linkWithPopup, TwitterAuthProvider } from "firebase/auth";
 import dynamic from "next/dynamic";
 import React from "react";
 
+const provider = new TwitterAuthProvider();
 import { SignInByWalletProps } from "../../molecules/SignInByWallet";
 
 const SignInByWallet = dynamic<SignInByWalletProps>(
@@ -12,13 +14,25 @@ const SignInByWallet = dynamic<SignInByWalletProps>(
 export type HeaderProps = BoxProps;
 
 export const Header: React.VFC<BoxProps> = ({ ...props }) => {
+  const connectTwitter = async () => {
+    const auth = getAuth();
+    if (!auth.currentUser) {
+      return;
+    }
+    const result = await linkWithPopup(auth.currentUser, provider);
+    console.log(result);
+  };
+
   return (
     <Box {...props}>
       <Flex minH="64px" alignItems="center" justifyContent="space-between" p="8">
         <Text color="gray.800" fontWeight="bold">
           NFTCert
         </Text>
-        <SignInByWallet />
+        <Flex gap="8px">
+          <Button onClick={connectTwitter}>Connect Twitter</Button>
+          <SignInByWallet />
+        </Flex>
       </Flex>
     </Box>
   );
